abcbda2d3970c8c7d06209758a2c00a6e50f2c7b
yxshao <simon0227@gmail.com>
2018-03-14 12:05:33 +0800

    add pairwise mode for visualization

--- src/inspector.py
+++ src/inspector.py
@@ -20,6 +20,8 @@ def read_data(path, data_type, args):
         model = Model.load_model(path)
         ent_vocab = Vocab.load(args.ent)
         rel_vocab = Vocab.load(args.rel)
+
+        valid_rels = np.random.randint(0, len(rel_vocab), args.n_cls)
         
         with open(args.kb) as f:
             for line in f:
@@ -27,8 +29,9 @@ def read_data(path, data_type, args):
                 sub_emb = model.pick_ent(ent_vocab[sub])
                 obj_emb = model.pick_ent(ent_vocab[obj])
                 rel_id = rel_vocab[rel]
-                X.append(sub_emb-obj_emb)
-                y.append(rel_id)
+                if (args.n_cls >= len(rel_vocab)) or (rel_id in valid_rels):
+                    X.append(sub_emb-obj_emb)
+                    y.append(rel_id)
     else:
         word_vector = KeyedVectors.load_word2vec_format(path, binary=False)
         ent_vocab = Vocab.load(args.ent)
@@ -46,16 +49,50 @@ def read_data(path, data_type, args):
                 sub_emb = pick_emb(ent_vocab[sub])
                 obj_emb = pick_emb(ent_vocab[obj])
                 rel_id = rel_vocab[rel]
-                X.append(sub_emb-obj_emb)
-                y.append(rel_id)
+                valid_rels = np.random.randint(0, len(rel_vocab), args.n_cls)
+                if (args.n_cls >= len(rel_vocab)) or (rel_id in valid_rels):
+                    X.append(sub_emb-obj_emb)
+                    y.append(rel_id)
 
     return np.array(X), np.array(y)
 
 def inspect(args):
     X, y = read_data(args.data, args.model, args)
+    
     if args.source == "WN18" and args.model == "deepwalk":
         y = y >= 40943 # label 1 for relation, label 0 for entity.
-    TwoDimProjection(X, y, args.source+"_"+args.model).visualize()
+    if args.vis_type == "pairwise":
+        s = len(set(y.tolist()))
+
+        def partX(X, y):
+            xx = {}
+            for idx in range(len(X)):
+                key = y[idx]
+                if key in xx:
+                    xx[key] = np.vstack((xx[key], X[idx]))
+                else:
+                    xx[key] = np.reshape(X[idx], (1, len(X[idx])))
+            return xx
+
+        part_X = partX(X, y)
+        # for idx in range(s):
+        #     print("idx:{} len:{}".format(idx, len(part_X[idx])))
+        # print("End")
+        for i in range(s):
+            for j in range(i+1, s):
+                # print(part_X[i].shape)
+                # print(part_X[j].shape)
+                a = np.vstack((part_X[i], part_X[j]))
+                xiy = np.array([k for k in range(len(part_X[i]))])
+                xjy = np.array([k for k in range(len(part_X[j]))])
+                b = np.hstack((xiy, xjy))
+                # print("i:{},j:{}".format(i,j))
+                # print(a.shape)
+                # print(b.shape)
+                TwoDimProjection(a,b,args.source+"_"+args.model+"_"+str(i)+"_"+str(j)).visualize()
+
+    else:
+        TwoDimProjection(X, y, args.source+"_"+args.model).visualize()
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser('Inspector for data analysis')
@@ -65,6 +102,8 @@ if __name__ == '__main__':
     p.add_argument('--ent', type=str, help='entity id list')
     p.add_argument('--rel', type=str, help='relation id list')
     p.add_argument('--kb', type=str, help='the knowledge base data')
+    p.add_argument('--n_cls', default=2, type=int, help='the number of different classes')
+    p.add_argument('--vis_type', default="all", type=str, help='the type for visualization')
 
     args = p.parse_args()
 
--- src/utils/data_visualize.py
+++ src/utils/data_visualize.py
@@ -14,11 +14,11 @@ class BasicVisualizer(object):
         raise NotImplementedError
 
     # simple scatter plot
-    def scatter_plot(self, X, y, s=0, flag="save"):
+    def scatter_plot(self, X, y, flag="save"):
         x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
         y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 
-        plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.6)
+        plt.scatter(X[:, 0], X[:, 1], s=1, c=y, alpha=0.6, cmap=plt.cm.Spectral)
         if flag == "save":
             plt.savefig(fname=self.name+".pdf")
         else:
